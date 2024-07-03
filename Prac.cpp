f6b777768f36d7dd8cad81c7a73f7a6561f41aef
TripelThreat <102275109+TripelThreat@users.noreply.github.com>
2024-07-03 22:22:19 +0700

    first commit

---
+++ Prac.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int num;
+    cout << "Enter an number: ";
+    cin >> num;
+    cout << "Got number: " << num;
+    return 0;
+}
---
+++ Week1.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    // ex1
+
+    // double a, b, x;
+    // cout << "Enter a: ";
+    // cin >> a;
+    // cout << "Enter b: ";
+    // cin >> b;
+    // x = -b / a;
+    // cout << "X is: " << x;
+
+    // ex2
+
+    // double a, b, c, d, e, A;
+    // cout << "Enter the number: ";
+    // cin >> a >> b >> c >> d >> e;
+    // // cout << a << b << c << d << e;
+    // A = (a + b + c + d + e) / 5;
+    // cout << "The avg numb is: " << A;
+
+    // ex3
+
+    string s = "Hello123 45abc";
+    for (auto &x : s)
+    {
+    }
+
+    return 0;
+}
