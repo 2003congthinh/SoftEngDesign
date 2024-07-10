16cf27bf58d76e2632a5a566336582992d8769a4
TripelThreat <102275109+TripelThreat@users.noreply.github.com>
2024-07-10 09:58:02 +0700

    cm

--- Week2.cpp
+++ Week2.cpp
@@ -2,6 +2,7 @@
 #include <string>
 #include <cmath>
 #include <stdexcept>
+#include <iomanip>
 using namespace std;
 
 int min(int x, int num[]){
@@ -75,5 +76,28 @@ int main(){
     // strToChar(str);
     
     // b)
-
+    //Precision
+    cout << 3.14159 << " " << 12.3456 << " (DATA TO DISPLAY)\n";
+    cout << "precision = 3: \n";
+    cout << std::setprecision(3)
+    << 3.14159 << " " << 12.3456 << "\n\n";
+    /* Note: setprecision(n) applies to the entire number,
+    must use the fixed-point format to apply to the fractional part only*/
+    cout << "precision = 3, fixed: \n";
+    cout << std::fixed << std::setprecision(3)
+    << 3.14159 << " " << 12.3456 << "\n\n\n";
+    //Width
+    cout << 10 << " " << 20 << " (DATA TO DISPLAY)\n";
+    cout << "width = 10: \n";
+    cout << std::setw(10)
+    << 10 << " " << 20 << "\n\n";
+    //Fill & Width
+    cout << "fill = 'x', width = 10: \n";
+    cout.fill('x');
+    cout.width(10);
+    cout << 10 << " " << 20 << "\n\n";
+    //Set Format Flags
+    cout << "set flags left justification and showpos, width = 10: \n";
+    cout << std::left << std::showpos << std::setw(10)
+    << 10 << " " << 20 << "\n\n";
 }
